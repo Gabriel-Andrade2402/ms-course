2fbeb747b11b8ec1bc300c15b2d9773384f221c9
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-09-14 08:51:14 -0300

    Project ht-worker created

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/WrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrWorkerApplication.class, args);
+	}
+
+}
