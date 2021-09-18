9ddbf114ef3a75ed90e83eeda0b146ba9eac770e
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-09-18 19:59:29 -0300

    Login, Jwt token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -1,20 +1,33 @@
 package com.devsuperior.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClients;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	@Autowired
 	private UserFeignClients userFeignClients;
 	
-	public User findByEmail(String email) {
+	/* Código antigo com feign comum sem implementação
+	 * public User findByEmail(String email) {
 		User user=userFeignClients.findByEmail(email).getBody();
 		if(user==null) {
 			throw new IllegalArgumentException();
+		}
+		return user;
+	}*/
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClients.findByEmail(username).getBody();
+		if (user == null) {
+			throw new UsernameNotFoundException("Email not found");
 		}
 		return user;
 	}
